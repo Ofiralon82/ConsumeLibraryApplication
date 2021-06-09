c68e5b0a71076b473520c3f9225897eb4b3b8708
ofiralon <ofir@bizzabo.com>
2021-06-09 13:52:04 +0300

    local and remote settings

--- app/src/main/java/com/example/consumerlibraryapplication/MainActivity.kt
+++ app/src/main/java/com/example/consumerlibraryapplication/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.consumerlibraryapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.libraryapplication.ConsumeMeObject
+import com.example.libraryapplication.MainActivity
+import com.cloudinary.*;
+import com.bumptech.glide.Glide
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ConsumeMeObject().pleaseConsumeMe("gfhd", 66)
+
+
+//        val em: Emitter = Emitter.EmitterBuilder("notarealuri.fake.io", this)
+//            .build()
+
     }
+
+//    override fun onResume() {
+//        super.onResume()
+//        val myIntent = Intent(this, MainActivity::class.java)
+//        startActivity(myIntent)
+//    }
 }
